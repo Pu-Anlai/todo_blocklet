5bd2ac757afc71d3a05564e958f6fb50fb765a17
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-05-09 23:16:27 +0200

    Add 'Task' keyword to blocklet output
    Include numeral checking: use singular if there's exactly one task,
    otherwise use plural.

--- todo_io.py
+++ todo_io.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python
 
-import re
 import colors
 import todo_rw
 import todo_ui
+
+
+# you can change these two to fit your own language (hopefully your language
+# doesn't have dual marking on nouns)
+tail_sgl_str = 'Task'
+tail_pl_str = 'Tasks'
+
+
+def tail(length):
+    if length == 1:
+        return tail_sgl_str
+    else:
+        return tail_pl_str
+
 
 task_dict = todo_rw.task_dict
 
 task_int = len(todo_rw.extract_bare_tasks(task_dict))
 urgent_int = len(task_dict['A'])
-block_out = '{0!s}<span font_size ="small"> (<span color="{1}">{2!s}</span>)</span>'\
-            .format(task_int, colors.color04, urgent_int)
+block_out = ( '{0!s}'
+              '<span font_size ="small">'
+              ' (<span color="{1}">{2!s}</span>)'
+              ' {3}</span>' ).format(task_int,
+                                     colors.color04,
+                                     urgent_int,
+                                     tail(task_int))
 
 print(block_out)
