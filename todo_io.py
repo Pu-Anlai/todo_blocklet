4f5b3f970e7d33afd7b89d34decf61ffa73035c5
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-05-10 01:04:48 +0200

    Skip urgent tasks if there aren't any

--- todo_io.py
+++ todo_io.py
@@ -14,17 +14,22 @@ def tail(length):
     else:
         return TAIL_PL_STR
 
+def critical_str(urgent_int, color):
+    '''Return a string for critical tasks only if there are any.'''
+    if urgent_int > 0:
+        return '(<span color="{0}">{1!s}</span>)'.format(color, urgent_int)
+    else:
+        return ''
 
 def print_blocklet(task_dict):
     '''Print the string that is shown as output in i3blocks.'''
     task_int = len(todo_rw.extract_bare_tasks(task_dict))
     urgent_int = len(task_dict['A'])
     block_out = ('{0!s}'
-                 '<span font_size ="small"> '
-                 '(<span color="{1}">{2!s}</span>) '
-                 '{3}</span>').format(task_int,
-                                      colors.color04,
-                                      urgent_int,
+                 '<span font_size ="small">'
+                 ' {1} '
+                 '{2}</span>').format(task_int,
+                                      critical_str(urgent_int, colors.color04),
                                       tail(task_int))
 
     print(block_out)
