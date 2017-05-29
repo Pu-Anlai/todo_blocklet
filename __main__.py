556c1e60b7d4550a864cfe8398367d530d394af7
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-05-29 23:51:20 +0200

    Add TODO list

--- __main__.py
+++ __main__.py
@@ -1,1 +1,6 @@
+# TODO: klasse an gui weitergeben
+# TODO: was soll passieren, wenn keine tasks vorhanden sind --> critical und non-critical
+# TODO: wenn keine todo.txt oder done.txt existieren: erstellen!
+# TODO: blocklet durch rechtsklick updaten
+
 import todo_io
