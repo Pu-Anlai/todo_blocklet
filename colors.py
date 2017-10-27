b4045318bcc82cae1d40000f86007711b6ecb874
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-10-28 00:40:48 +0200

    Implement entry box for adding tasks

--- colors.py
+++ colors.py
@@ -1,6 +1,15 @@
+# support global color settings for all blocklets set in a bash script one
+# level up from todo_blocklet
+
 import imp
 import os
 WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
 UP_DIR = os.path.abspath(os.path.join(WORKING_DIR, '..'))
-imp.load_source('colors', '{}/colors'.format(UP_DIR))
-from colors import * # i think this is a case where it's okay to do this
+try:
+    imp.load_source('colors', '{}/colors'.format(UP_DIR))
+    from colors import *
+except FileNotFoundError:
+    color01 = "#a3be8c"
+    color02 = "#ebcb8b"
+    color03 = "#d08770"
+    color04 = "#bf616a"
